ec55d4c5152236c719eb5832dfd82d5bb9b4b8e6
Ssebv <sallendec@uft.edu>
2023-09-25 16:05:24 -0300

    Final Final Final

--- main.py
+++ main.py
@@ -26,14 +26,14 @@ def generate_key(): # Función para generar una llave aleatoria
     clave = Fernet.generate_key()
     return clave # Generar una llave aleatoria
 
-def encrypt(text, key): # Función para cifrar un texto
+def encrypting(text, key): # Función para cifrar un texto
     try:
         f = Fernet(key) # Crear un objeto Fernet de cifrado simétrico
         return f.encrypt(text.encode()) # Cifrar a los bytes del texto plano
     except:
         return "ERROR: No se pudo cifrar el texto"
 
-def decrypt(ciphertext, key): # Función para descifrar un texto
+def decrypting(ciphertext, key): # Función para descifrar un texto
     try:
         f = Fernet(key) # Crear un objeto Fernet
         decrypted = f.decrypt(ciphertext.encode()).decode() # Descifrar el texto cifrado y convertirlo a texto plano con decode()
@@ -47,15 +47,15 @@ def main(): # Función principal
     output_file = "mensajeseguro.txt" # Nombre del archivo de salida
     plaintext = read_txt(input_file)
     
+    print("Prueba de cifrado simétrico")
     print("----------------------------------------")
     print("[+] Texto plano:", plaintext)
     
     if plaintext is not None: # Si el archivo no está vacío
         key = generate_key() # Generar una llave aleatoria 
-        print("[+] Llave generada:", key)
         
-        hash_original = get_hash(plaintext).encode()  # Convertir el hash a bytes
-        ciphertext = encrypt(plaintext, key)  # Cifrar el texto plano
+        hash_original = get_hash(plaintext).encode()  # Convertir el hash a bytes para interpretar si se modifico el archivo
+        ciphertext = encrypting(plaintext, key)  # Cifrar el texto plano
         print("[+] Texto cifrado:", ciphertext)  
         write_txt(output_file, ciphertext + b'\n' + hash_original) # Escribir el texto cifrado en un archivo
 
@@ -67,7 +67,7 @@ def main(): # Función principal
         current_ciphertext = read_txt(output_file) # Leer el archivo cifrado 
         print("[+] Texto cifrado:", current_ciphertext) 
         
-        decrypted = decrypt(current_ciphertext, key) # Descifrar el texto cifrado
+        decrypted = decrypting(current_ciphertext, key) # Descifrar el texto cifrado
         print("[+] Texto descifrado:", decrypted)
         
         print("\n----------------------------------------")
@@ -85,5 +85,6 @@ def main(): # Función principal
         
 if __name__ == "__main__": # Función para ejecutar el programa
     main()
+    print("Fin del programa ... ")
     
     
